530498bcdde936b3495c91ef1f51be02eb9a04b1
Ankit <ankitvika02@gmail.com>
2024-12-14 01:15:18 +0530

    new one

--- src/controllers/userControllers/logout.controllers.js
+++ src/controllers/userControllers/logout.controllers.js
@@ -2,13 +2,12 @@ import ApiResponse from "../../utils/helper/ApiResponse.js";
 import { asyncHandler } from "../../utils/helper/AsyncHandler.js";
 
 const logout = asyncHandler(async (req, res) => {
-    res.clearCookie("accessToken")
-    // res.cookie('accessToken', '', {
-    //     expires: new Date(0), // Expire the cookie immediately
-    //     httpOnly: true,
-    //     secure: true, // if using HTTPS
-    //     sameSite: 'None', // ensure consistency with the original cookie settings
-    //   });
+    // res.clearCookie("accessToken")
+    res.clearCookie('accessToken', '', {
+        httpOnly: true,
+        secure: true, // if using HTTPS
+        sameSite: 'None', // ensure consistency with the original cookie settings
+      });
     return res.status(200)
         .json(
             new ApiResponse(
